74d31aacd6a033f0cdb0911d55100b0352ce1b20
SylvanaMelens <sylvana.melens@hotmail.com>
2020-05-08 22:14:42 +0200

    maj

--- src/App.js
+++ src/App.js
@@ -2,53 +2,47 @@
  * site web-carte de visite en REACT
  * @author: sylvana
  */
-import React, { Component } from 'react'
-import './App.css'
-import Presentation from './components/Presentation'
-import CV from './components/CV'
-import Portfolio from './components/Portfolio'
-import Error from './components/Error'
-import NavbarHeader from './containers/NavbarHeader'
-import Footer from './containers/Footer'
-import Banner from './containers/Banner'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-
+import React, { Component } from "react";
+import "./App.css";
+import Presentation from "./components/Presentation";
+import CV from "./components/CV";
+import Portfolio from "./components/Portfolio";
+import Error from "./components/Error";
+import NavbarHeader from "./containers/NavbarHeader";
+import Footer from "./containers/Footer";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 export default class App extends Component {
-  constructor(props){
-    super(props)
-    console.log('isShow dans app', this.props.isShow)
+  constructor(props) {
+    super(props);
+    console.log("isShow dans app", this.props.isShow);
     this.state = {
-      isShow : true
-    }
+      isShow: true,
+    };
   }
 
   handleClick = (e) => {
-      this.setState({ isShow : false })
-      console.log('click sur accepter')
-  }
+    this.setState({ isShow: false });
+    console.log("click sur accepter");
+  };
 
-  render(){
+  render() {
     return (
-      
       <div className="container-fluid">
         <BrowserRouter>
-            <div className="container-fluid App-header">
-              <NavbarHeader />
-              <Switch className="Content">
-                <Route exact path="/" component={Presentation} /> 
-                <Route path="/presentation" component={Presentation} />
-                <Route path="/cv" component={CV} />
-                <Route path="/portfolio" component={Portfolio} />
-                <Route path="" component={Error} />
-              </Switch>
-            </div>
-        </BrowserRouter>  
-        {this.state.isShow ? <Banner onClick={this.handleClick}/> : null}
+          <div className="container-fluid App-header">
+            <NavbarHeader />
+            <Switch className="Content">
+              <Route exact path="/" component={Presentation} />
+              <Route path="/presentation" component={Presentation} />
+              <Route path="/cv" component={CV} />
+              <Route path="/portfolio" component={Portfolio} />
+              <Route path="" component={Error} />
+            </Switch>
+          </div>
+        </BrowserRouter>
         <Footer />
       </div>
-    )
+    );
   }
 }
-
-
--- src/ListPortfolio.js
+++ src/ListPortfolio.js
@@ -1,32 +1,32 @@
 export default {
-    image0: {
-      title: 'Mon premier site',
-      src: 'sitesylvanamelens.png',
-      outerLink: 'https://sleepy-curran-6a615f.netlify.com',
-      desc: 'Site développé "from scratch" en HTML-CSS et Bootstrap'
-    },
-    image1: {
-      title: 'craboudja.com',
-      src: 'craboudja.png',
-      outerLink: 'https://craboudja.com',
-      desc: 'Blog Wordpress'
-    },
-    image2: {
-      title: 'Site vitrine fictif',
-      src: 'haircoiffure.png',
-      outerLink: 'https://haircoiffure.netlify.com',
-      desc: 'Basic template (Gatsby.js/Netlify)'
-    },
-    image3: {
-      title: 'Projet en cours',
-      src: 'ecole.png',
-      outerLink: '',
-      desc: 'Site école de Ferrières, en React.js '
-    },
-    image4: {
-      title: 'A venir',
-      src: 'bulles.jpg',
-      outerLink: 'mailto::sylvana.melens@hotmail.com',
-      desc: 'Peut-être votre site ?'
-    }
-}
+  image0: {
+    title: "Mon premier site",
+    src: "sitesylvanamelens.png",
+    outerLink: "https://sylvanamelensbe.netlify.app",
+    desc: 'Site développé "from scratch" en HTML-CSS et Bootstrap',
+  },
+  image1: {
+    title: "craboudja.com",
+    src: "craboudja.png",
+    outerLink: "https://craboudja.com",
+    desc: "Blog Wordpress",
+  },
+  image2: {
+    title: "Site vitrine fictif",
+    src: "haircoiffure.png",
+    outerLink: "https://haircoiffure.netlify.com",
+    desc: "Basic template (Gatsby.js/Netlify)",
+  },
+  image3: {
+    title: "Projet en cours",
+    src: "ecole.png",
+    outerLink: "",
+    desc: "Site école de Ferrières, en React.js ",
+  },
+  image4: {
+    title: "A venir",
+    src: "bulles.jpg",
+    outerLink: "mailto::sylvana.melens@hotmail.com",
+    desc: "Peut-être votre site ?",
+  },
+};
