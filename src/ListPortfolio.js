0c852b545af9866f71f6363832615f8aa6c7821f
SylvanaMelens <sylvana.melens@hotmail.com>
2020-06-25 22:03:35 +0200

    update

--- src/ListPortfolio.js
+++ src/ListPortfolio.js
@@ -1,39 +1,32 @@
 export default {
   image0: {
-    title: "Mon premier site",
-    src: "sitesylvanamelens.png",
-    outerLink: "https://sylvanamelensbe.netlify.app",
-    desc: 'Site développé "from scratch" en HTML-CSS et Bootstrap',
+    title: "Jeu de réflexion",
+    src: "code4couleurs.png",
+    outerLink: "https://trouvelecodede4couleurs.netlify.app/",
+    desc: "Petit jeu en HTML/CSS/JAVASCRIPT",  
   },
   image1: {
-    title: "craboudja.com",
-    src: "craboudja.png",
-    outerLink: "https://craboudja.com",
-    desc: "Blog Wordpress",
-  },
-  image2: {
-    title: "Site vitrine fictif",
-    src: "haircoiffure.png",
-    outerLink: "https://haircoiffure.netlify.com",
-    desc: "Basic template (Gatsby.js/Netlify)",
-  },
-  image3: {
     title: "Projet en cours",
     src: "ecole.png",
     outerLink: "https://efwb-ferrieres-projet-en-cours.netlify.app/",
     desc: "Site école de Ferrières, en React.js ",
   },
+  image2: {
+    title: "craboudja.com",
+    src: "craboudja.png",
+    outerLink: "https://craboudja.com",
+    desc: "Blog Wordpress",
+  },
+  image3: {
+    title: "Site vitrine fictif",
+    src: "haircoiffure.png",
+    outerLink: "https://haircoiffure.netlify.com",
+    desc: "Basic template (Gatsby.js/Netlify)",
+  },
   image4: {
-    title: "Jeu de réflexion",
-    src: "code4couleurs.png",
-    outerLink: "https://trouvelecodede4couleurs.netlify.app/",
-    desc: "Petit jeu en HTML/CSS/JAVASCRIPT",
+    title: "Mon premier site",
+    src: "sitesylvanamelens.png",
+    outerLink: "https://sylvanamelensbe.netlify.app",
+    desc: 'Site développé "from scratch" en HTML-CSS et Bootstrap',
   },
-  image5: {
-    title: "A venir",
-    src: "bulles.jpg",
-    outerLink: "mailto::sylvana.melens@hotmail.com",
-    desc: "Peut-être votre site ?",
-  },
-
 };
--- src/containers/Text.js
+++ src/containers/Text.js
@@ -3,19 +3,43 @@
  * @author: sylvana
  */
 
-import React from 'react'
-import './Text.css'
+import React from "react";
+import "./Text.css";
 
 const Text = () => {
-    return (
-        <div className='text'>
-            <p>Après 20 années d'expérience en tant que secrétaire juridique et comptable, j'ai autorisé mon côté geek à prendre le dessus et ai décidé de m'auto-former dans l'informatique. </p>
-            <p>Depuis presque deux ans, je me passionne pour le <strong>développement web</strong> et je code dès que cela est possible.</p> 
-            <p>Ma détermination face à l'inconnu est devenue une force, que je suis fière d'arborer et de mettre au service de mes clients.</p>
-            <p>Actuellement, j'aime beaucoup développer en <strong>React.js</strong>, pour son approche orientée "composants", mais je reste à l'écoute de tout type de projets.</p>
-            <p>Pour information, le code de ce site est consultable sur mon <a href='https://github.com/SylvanaMelens' target='_blank' rel='noopener noreferrer'>Github</a></p>
-        </div>
-    )
-}
+  return (
+    <div className="text">
+      <p>
+        Après 20 années d'expérience en tant que secrétaire juridique et
+        comptable, j'ai autorisé mon côté geek à prendre le dessus et ai décidé
+        de me diriger vers l'informatique.{" "}
+      </p>
+      <p>
+        Depuis presque deux ans, je me passionne pour le{" "}
+        <strong>développement web</strong> et je code dès que cela est possible.
+      </p>
+      <p>
+        Ma détermination face à l'inconnu est devenue une force, que je suis
+        fière d'arborer.
+      </p>
+      <p>
+        Côté front, j'aime beaucoup développer en <strong>React.js</strong>,
+        mais je suis capable de m'adapter à tout type de projets, facilement et
+        volontiers.
+      </p>
+      <p>
+        N'hésitez pas à faire un tour sur mon{" "}
+        <a
+          href="https://github.com/SylvanaMelens"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a>{" "}
+        afin de vous faire une petite idée.
+      </p>
+    </div>
+  );
+};
 
-export default Text
+export default Text;
